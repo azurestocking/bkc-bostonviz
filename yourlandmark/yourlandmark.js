f0b059bb5d9badebdf8f2b486b3c3d954e35d49a
miw677 <108368372+miw677@users.noreply.github.com>
2024-03-28 21:18:06 -0400

    Update yourlandmark.js

--- yourlandmark/yourlandmark.js
+++ yourlandmark/yourlandmark.js
@@ -1,31 +1,32 @@
+let marker;
+let displayName = "";
+
+// Set map boundaries
 var bounds = [
     [41.237964, -73.508142], // Southwestern coordinates
     [42.886589, -69.858861]  // Northeastern coordinates
 ];
 
-
+// Initialize map element
 let mymap = L.map('mapid', {
     zoomControl: false,
     maxBounds: bounds,
     maxBoundsViscosity: 1.0
 }).setView([42.3601, -71.0589], 13); // Boston's coordinates
+
+// Set zoom control position
 L.control.zoom({position: 'bottomleft'}).addTo(mymap);
 
+// Set high definition basemap
 L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}@2x.png', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
 
-let marker;
-let displayName = "";
-
-
 // Initialize the marker at map center
 function addInitialMarker() {
-    // let name = document.getElementById('name').value;
     marker = L.marker(mymap.getCenter()).addTo(mymap);
     marker.bindPopup("Move map or search to set your landmark.").openPopup();
 }
-
 
 addInitialMarker();
 
@@ -34,21 +35,7 @@ mymap.on('move', function () {
     marker.setLatLng(mymap.getCenter());
 });
 
-
-// mymap.addControl(new L.Control.Search({
-//     layer: landmarksLayer,
-//     initial: false,
-//     propertyName: 'title',
-//     marker: false,
-//     moveToLocation: function(latlng, title, map) {
-//         map.setView(latlng, 17);
-//         let marker = L.marker(latlng).addTo(map);
-//         marker.bindPopup(title).openPopup();
-//     }
-// }));
-
-
-// Define the control options in a variable
+// Initialize search control options
 let searchControlOptions = {
     position: 'topleft',
     autoCollapse: false,
@@ -60,12 +47,16 @@ let searchControlOptions = {
     jsonpParam: "json_callback",
     marker: null,
     minLength: 2,
-    moveToLocation: function(location) {
+    moveToLocation: function(location) { // Action set after search result
         mymap.fitBounds(location.__boundingBox__);
-        console.log("Location Name inside moveToLocation:", location.display_name);
+        //console.log("Location Name in moveToLocation:", location.display_name);
         displayName = location.display_name;
-        var displayNamePop = location.display_name ? location.display_name : "Your New Landmark";
-        marker.bindPopup(displayNamePop).openPopup();
+        function onMoveEnd() {
+            var displayNamePop = location.display_name ? location.display_name : "Your New Landmark";
+            marker.bindPopup(displayNamePop).openPopup(); // Popup with location name
+            mymap.off('moveend', onMoveEnd);
+        }
+        mymap.on('moveend', onMoveEnd);
     },
     propertyLoc: ["lat", "lon"],
     propertyName: "display_name",
@@ -76,49 +67,8 @@ let searchControlOptions = {
 // Create and add the search control to the map
 let searchControl = new L.Control.Search(searchControlOptions);
 mymap.addControl(searchControl);
-console.log(searchControlOptions.propertyName);
 
-// mymap.addControl(new L.Control.Search({
-//     autoCollapse: false,
-//     autoResize: false,
-//     autoType: false,
-//     collapsed: false,
-//     firstTipSubmit: true,
-//     formatData: processSearchResults,
-//     jsonpParam: "json_callback",
-//     marker: null,
-//     minLength: 2,
-//     moveToLocation: function(location) {
-//         mymap.fitBounds(location.__boundingBox__);
-//     },
-//     propertyLoc: ["lat", "lon"],
-//     propertyName: "display_name",
-//     textPlaceholder: "Search for a place...",
-//     url: "https://nominatim.openstreetmap.org/search?format=json&q={s} Massachusetts, United States"
-// }));
-
-
-
-
-
-function submitLandmark() {
-    // var name = document.getElementById('name').value;
-    // var description = document.getElementById('description').value;
-    var latlng = marker.getLatLng();
-    var name = displayName;
-
-    // console.log('Name:', name);
-    // console.log('Description:', description);
-    console.log('Latitude:', latlng.lat, 'Longitude:', latlng.lng, 'Display Name:', displayName);
-    window.location.href = `https://lobs.boston/bostonlandmark/submission.php?name=${name}&latitude=${latlng.lat}&longitude=${latlng.lng}`;
-
-    // Here, you would send this data to your server via AJAX or a form submission
-}
-
-
-
-// TEST FIELD
-
+// Process search results
 function processSearchResults(searchResults) {
     console.log("Search Results:", searchResults);
 
@@ -133,10 +83,23 @@ function processSearchResults(searchResults) {
         ];
         processedResults[locationName].display_name = location.name;
     }
-    console.log("Processed Results:", processedResults);
+    // console.log("Processed Results:", processedResults);
 
     return processedResults;
 }
 
+// Continue to next step action set
+function submitLandmark() {
+    var latlng = marker.getLatLng();
+    var name = displayName;
+    console.log('Latitude:', latlng.lat, 'Longitude:', latlng.lng, 'Display Name:', displayName);
+    window.location.href = `https://lobs.boston/bostonlandmark/submission.php?name=${name}&latitude=${latlng.lat}&longitude=${latlng.lng}`;
+}
 
+// Click event listener
 document.getElementById('submitLandmarkBtn').addEventListener('click', submitLandmark);
+
+// iOS fix: Listen for the 'movestart' event on the map
+mymap.on('movestart', function() {
+    mymap.closePopup(); // Close any open popup
+});
