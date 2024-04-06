3e47acba57ce27985158376b6df1fe8dfc633446
azurestocking <ljm97902@gmail.com>
2024-04-06 18:28:15 -0400

    add navbar

--- js/landmarkMap.js
+++ js/landmarkMap.js
@@ -58,7 +58,6 @@ class LandmarkMap {
 		// display writeup
 		if (selectedCategory === "official-landmarks") {
 			document.getElementById('nav').innerHTML = `
-                <h1 class="row">OFFICIAL LANDMARKS</h1>
                 <p class="row">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
@@ -70,7 +69,6 @@ class LandmarkMap {
             `;
 		} else if (selectedCategory === "your-landmarks") {
 			document.getElementById('nav').innerHTML = `
-                <h1 class="row">YOUR LANDMARKS</h1>
                 <p class="row">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
