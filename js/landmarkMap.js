d66a085d1b68c4c1095da00fe03275bef46ca749
azurestocking <ljm97902@gmail.com>
2024-04-11 22:20:31 -0400

    update sidebar

--- js/landmarkMap.js
+++ js/landmarkMap.js
@@ -66,7 +66,7 @@ class LandmarkMap {
 				  	<div class="accordion-item">
 						<h2 class="accordion-header">
 							<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-								<b>STEP 1</b>
+								<b>How to Apply</b>
 							</button>
 						</h2>
 						<div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
@@ -78,7 +78,7 @@ class LandmarkMap {
 				  	<div class="accordion-item">
 						<h2 class="accordion-header">
 							<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-								<b>STEP 2</b>
+								<b>Who Decides</b>
 							</button>
 						</h2>
 						<div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
@@ -88,16 +88,16 @@ class LandmarkMap {
 						</div>
 				    </div>
 				    <div class="accordion-item">
-				        <h2 class="accordion-header">
-						  	<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-								<b>STEP 3</b>
-						  	</button>
-					    </h2>
-					    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-					  	  	<div class="accordion-body">
+						<h2 class="accordion-header">
+							<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseTwo">
+								<b>What Criteria</b>
+							</button>
+						</h2>
+						<div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+							<div class="accordion-body">
 								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-					  	  	</div>
-					    </div>
+							</div>
+						</div>
 				    </div>
 				</div>
             `;
@@ -107,7 +107,7 @@ class LandmarkMap {
 				  	<div class="accordion-item">
 						<h2 class="accordion-header">
 							<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-								<b>STEP 1</b>
+								<b>How to Apply</b>
 							</button>
 						</h2>
 						<div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
@@ -119,7 +119,7 @@ class LandmarkMap {
 				  	<div class="accordion-item">
 						<h2 class="accordion-header">
 							<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-								<b>STEP 2</b>
+								<b>Who Decides</b>
 							</button>
 						</h2>
 						<div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
@@ -131,7 +131,7 @@ class LandmarkMap {
 				    <div class="accordion-item">
 				        <h2 class="accordion-header">
 						  	<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-								<b>STEP 3</b>
+								<b>What Criteria</b>
 						  	</button>
 					    </h2>
 					    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
